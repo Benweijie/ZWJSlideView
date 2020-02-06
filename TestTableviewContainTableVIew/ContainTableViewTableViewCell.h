8e285024b4a26b38ec36104f27f57c2d25a81baa
zhongweijie <zhongweijie@kuaishou.com>
2020-02-06 18:12:16 +0800

    init

---
+++ TestTableviewContainTableVIew/ContainTableViewTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  ContainTableViewTableViewCell.h
+//  TestTableviewContainTableVIew
+//
+//  Created by 钟伟杰 on 2020/2/6.
+//  Copyright © 2020 钟伟杰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ContainTableViewTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ TestTableviewContainTableVIew/ContainTableViewTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  ContainTableViewTableViewCell.m
+//  TestTableviewContainTableVIew
+//
+//  Created by 钟伟杰 on 2020/2/6.
+//  Copyright © 2020 钟伟杰. All rights reserved.
+//
+
+#import "ContainTableViewTableViewCell.h"
+
+@implementation ContainTableViewTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
